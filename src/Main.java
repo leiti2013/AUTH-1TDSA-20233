3fa0767899aa53d96a6f8a016d4e6b575eb1d008
leiti2013 <leiticontato@hotmail.com>
2023-05-19 02:25:42 -0300

    finalmente

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,101 @@
+
+
+import model.Sistema.Sistema;
+import model.autenticacao.Profile;
+import model.autenticacao.Role;
+import model.autenticacao.User;
+import model.pessoa.Pessoa;
+import model.pessoa.PessoaFisica;
+import model.pessoa.PessoaJuridica;
+
+import javax.swing.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
+    public static PessoaFisica novaPessoa(Long id, String nome, LocalDate nascimento, String cpf) {
+        PessoaFisica pf = new PessoaFisica();
+        pf.setNome(nome);
+        pf.setCpf(cpf);
+        pf.setId(id);
+        pf.setNascimento(nascimento);
+        return pf;
     }
+
+    public static PessoaJuridica novaPessoaJuridica(Long id, String nome, LocalDate nascimento, String cnpj) {
+        PessoaJuridica pj = new PessoaJuridica();
+        pj.setNome(nome);
+        pj.setNascimento(nascimento);
+        pj.setId(id);
+        pj.setCnpj(cnpj);
+        return pj;
+    }
+
+    public static User novoUsuario(Long id, String email, String password, Pessoa pessoa) {
+        User login = new User();
+        login.setPessoa(pessoa);
+        login.setId(id);
+        login.setEmail(email);
+        login.setPassword(password);
+        return login;
+    }
+
+    public static Profile novoProfile(Long id, String nome) {
+        Profile profile = new Profile();
+        profile.setId(id);
+        profile.setNome(nome);
+        return profile;
+    }
+
+    public static Role novaRole(Long id, String nome, String descricao, Sistema sistema) {
+        Role rl = new Role();
+        rl.setId(id);
+        rl.setNome(nome);
+        rl.setDescricao(descricao);
+        return rl;
+    }
+
+    public static Sistema novoSistema(Long id, String nome, String sigla) {
+        Sistema sistema = new Sistema();
+        sistema.setId(id);
+        sistema.setNome(nome);
+        sistema.setSigla(sigla);
+        return sistema;
+    }
+
+    public static void main(String[] args) {
+        PessoaFisica leo = novaPessoa(1L,"leo",LocalDate.of(2005,1,9),"111111111");
+        PessoaJuridica holding = novaPessoaJuridica(2L,"Holding",LocalDate.of(2021,3,15),"111111");
+        User leol = novoUsuario(3L,"leo@hotmail.com","senha",leo);
+        Profile gerente = novoProfile(4L,"leo");
+        Sistema mercado = novoSistema(5L,"fate","fg");
+
+        PessoaFisica brabo = novaPessoa(732L,"leo filho", LocalDate.of(2023,8,15),"2342345");
+
+        leo.addFilho(brabo);
+        holding.addSocio(leo);
+        leol.addProfile(gerente);
+        mercado.addResponsavel(holding);
+        int sucesso = 0;
+
+        do {
+
+            var login = JOptionPane.showInputDialog("Digite seu email: ");
+            var senha = JOptionPane.showInputDialog("Digite sua senha: ");
+
+            if (Objects.equals(login, leol.getEmail()) && Objects.equals(senha, leol.getPassword())) {
+                System.out.println("Você logou!!");
+                sucesso = 1;
+            } else {
+                System.out.println("Email ou senha incorretos Falha no login!");
+                sucesso = 0;
+            }
+
+        }while (sucesso != 1);
+    }
+
+
+
+
 }
---
+++ src/model/pessoa/PessoaFisica.java
@@ -0,0 +1,51 @@
+package model.pessoa;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Vector;
+
+public class PessoaFisica extends Pessoa {
+
+    private String cpf;
+    private Collection<PessoaFisica> filhos = new Vector<>();
+
+    public PessoaFisica addFilho(PessoaFisica filho) {
+        this.filhos.add(filho);
+        return this;
+    }
+
+    public PessoaFisica removeFilho(PessoaFisica filho) {
+        this.filhos.remove(filho);
+        return this;
+    }
+
+    public  Collection<Pessoa> getFilhos(){return Collections.unmodifiableCollection(this.filhos);
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public PessoaFisica() {
+    }
+
+    public PessoaFisica(long id, String nome, LocalDate nascimento, String cpf, Collection<PessoaFisica> filhos) {
+        super(id, nome, nascimento);
+        this.cpf = cpf;
+        this.filhos = filhos;
+    }
+
+    @Override
+    public String toString() {
+        return "PessoaFisica{" +
+                "cpf='" + cpf + '\'' +
+                ", filhos=" + filhos +
+                "} " + super.toString();
+    }
+}
